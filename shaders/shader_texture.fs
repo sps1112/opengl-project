39687bf322db0b6a275a8eb6ef60622e1f46c5b8
sps1112 <sidps1112@gmail.com>
2021-01-19 01:55:43 +0530

    Update standard texture shader

--- shaders/shader_texture.fs
+++ shaders/shader_texture.fs
@@ -10,6 +10,7 @@ struct Material
 struct Light
 {
     vec3 position;
+    
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -42,6 +43,7 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
     vec3 specular = light.specular * (spec * (vec3(texture(material.specular,TexCoords))));
 
+    // Emmision Lighting
     vec3 emmision=vec3(0.0f);
     if(texture(material.specular,TexCoords).r==0.0)
     {
