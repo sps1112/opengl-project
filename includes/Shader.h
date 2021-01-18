22b7a7aec7e5ec038823a721555f23eeeff9023e
sps1112 <sidps1112@gmail.com>
2021-01-18 22:20:13 +0530

    Update Shader header

--- includes/Shader.h
+++ includes/Shader.h
@@ -89,7 +89,7 @@ public:
 	}
 	void setFloat(const std::string &name, float value) const
 	{
-		glUniform1f(glGetUniformLocation(ID, name.c_str()), (int)value);
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
 	void setVec2(const std::string &name, const glm::vec2 &value) const
 	{
