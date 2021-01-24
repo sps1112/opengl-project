e012f6eb126b7dff3887bcc51c57f55e006df1ec
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-01-24 21:22:38 +0530

    Update files for windows path

--- configuration/root_directory.h
+++ configuration/root_directory.h
@@ -1,2 +1,8 @@
 // const char *logl_root = "${CMAKE_SOURCE_DIR}";
+#ifdef __linux__
 const char *logl_root = "/home/sidps1112/Desktop/opengl-project";
+#elif _WIN32
+const char *logl_root = "C:/Users/Siddhartha/Desktop/opengl-project";
+#else
+const char *logl_root = "/home/sidps1112/Desktop/opengl-project";
+#endif
