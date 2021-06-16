53c861573375564407a82e81c68bdd694ac4b078
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-06-17 00:56:04 +0530

    Add default shader types

--- includes/rendering/Material.h
+++ includes/rendering/Material.h
@@ -4,6 +4,7 @@
 #include <utility/Utils.h>
 #include <utility/CustomMath.h>
 #include <rendering/Texture.h>
+#include <config.h>
 
 using Colorf = Vec3;
 
@@ -14,6 +15,7 @@ enum SHADER_TYPE
     TEXTURE_2D,
     LIGHT_SHADER,
     COLOR_3D,
+    COLR_3D_MAT,
     TEXTURE_3D,
     MODEL_3D,
     FBO_SHADER,
@@ -24,18 +26,9 @@ enum SHADER_TYPE
 };
 
 // File Name for Shader Files
-const std::string shader_type_strings[] = {
-    "shader_2d_color",
-    "shader_2d_tex",
-    "shader_light",
-    "shader_3d_color",
-    "shader_3d_tex",
-    "shader_3d_model",
-    "shader_fbo",
-    "shader_skybox",
-    "shader_blend",
-    "shader_reflect",
-    "shader_refract"};
+extern std::string shader_type_strings[12];
+
+extern std::string shader_directory;
 
 // Material Struct
 struct Material
@@ -43,9 +36,17 @@ struct Material
     Colorf albedo;
     Colorf emmision;
     SHADER_TYPE type;
-
+    Material()
+        : albedo(COLOR_GRAY), emmision(COLOR_BLACK), type(COLOR_2D) {}
     Material(Colorf albedo_, Colorf emmision_, SHADER_TYPE type_)
         : albedo(albedo_), emmision(emmision_), type(type_) {}
+    Material(const Material &mat)
+        : albedo(mat.albedo), emmision(mat.emmision), type(mat.type) {}
 };
+const Material defaultMat2D(Colorf(DEFAULT_SHADER_COLOR), Colorf(COLOR_BLACK), COLOR_2D);
+const Material defaultMat3D(Colorf(DEFAULT_SHADER_COLOR), Colorf(COLOR_BLACK), COLOR_3D);
+
+std::string GetVSPath(SHADER_TYPE type);
+std::string GetFSPath(SHADER_TYPE type);
 
 #endif
--- src/rendering/Material.cpp
+++ src/rendering/Material.cpp
@@ -1,1 +1,27 @@
 #include <rendering/Material.h>
+
+std::string GetVSPath(SHADER_TYPE type)
+{
+    return (shader_directory + shader_type_strings[type] + ".vs");
+}
+
+std::string GetFSPath(SHADER_TYPE type)
+{
+    return (shader_directory + shader_type_strings[type] + ".fs");
+}
+
+std::string shader_type_strings[] = {
+    "shader_2d_color",
+    "shader_2d_tex",
+    "shader_light",
+    "shader_3d_color",
+    "shader_3d_mat",
+    "shader_3d_tex",
+    "shader_3d_model",
+    "shader_fbo",
+    "shader_skybox",
+    "shader_blend",
+    "shader_reflect",
+    "shader_refract"};
+
+std::string shader_directory = "shaders/final/";
