cc511f8d5c5f755dc87ccdd96614f44ee397bdf3
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-06-17 00:57:05 +0530

    Add template actors

--- includes/object/Actor.h
+++ includes/object/Actor.h
@@ -37,6 +37,8 @@ private:
 class CameraActor : public Actor
 {
 public:
+    CameraActor();
+
 private:
     Camera cam;
 };
@@ -46,7 +48,10 @@ class RenderActor : public Actor
 {
 public:
     Material mat;
-    void Draw();
+    std::string path;
+    bool isVisible;
+    RenderActor();
+    RenderActor(std::string name_, ACTOR_TYPES type_, std::string path_ = "");
 
 private:
 };
@@ -54,22 +59,47 @@ private:
 class PrimitiveActor : public RenderActor
 {
 public:
-private:
-    Primitive prm;
+    PrimitiveActor(std::string name_, std::string path_ = "");
 };
 
 class ModelActor : public RenderActor
 {
 public:
-private:
-    Model mod;
+    ModelActor(std::string name_, std::string path_ = "");
 };
 
 class LightActor : public RenderActor
 {
 public:
-private:
-    Light light;
+    LightActor();
 };
 
+// Default Actors
+enum TEMPLATE_ACTORS
+{
+    TRIANGLE_2D,
+    RECTANGLE_2D,
+    QUAD_2D,
+    CUBE_3D,
+    QUAD_3D,
+    PLANE_3D,
+    CUBEMAP_3D,
+    BACKPACK_MODEL,
+    SPHERE_MODEL,
+    NORMAL_LIGHT_ACTOR,
+    POINT_LIGHT_ACTOR,
+    DIR_LIGHT_ACTOR,
+    SPT_LIGHT_ACTOR,
+    CAMERA_OBJECT_ACTOR,
+};
+
+extern ACTOR_TYPES actor_types[14];
+
+extern std::string resource_dir;
+
+extern std::string template_actor_filepath[14];
+
+extern SHADER_TYPE default_actor_shaders[14];
+
+extern unsigned int default_actor_id[14];
 #endif // !ACTOR_H
--- src/object/Actor.cpp
+++ src/object/Actor.cpp
@@ -1,3 +1,111 @@
 #include <object/Actor.h>
 
-Actor::Actor(std::string name_, Transform transform_) : name(name_), transform(transform_) {}
+Actor::Actor(std::string name_, Transform transform_)
+    : name(name_), transform(transform_) {}
+
+CameraActor::CameraActor()
+{
+}
+
+RenderActor::RenderActor()
+{
+    isVisible = true;
+    mat = Material(defaultMat2D);
+}
+
+RenderActor::RenderActor(std::string name_, ACTOR_TYPES type_, std::string path_)
+{
+    name = name_;
+    transform = Transform();
+    type = type_;
+    path = path_;
+    isVisible = true;
+    mat = Material(DEFAULT_SHADER_COLOR, COLOR_BLACK, default_actor_shaders[type]);
+}
+
+PrimitiveActor::PrimitiveActor(std::string name_, std::string path_)
+{
+    name = name_;
+    transform = Transform();
+    type = PRIMITIVE_ACTOR;
+    path = path_;
+}
+
+ModelActor::ModelActor(std::string name_, std::string path_)
+{
+    name = name_;
+    transform = Transform();
+    type = MODEL_ACTOR;
+    path = path_;
+}
+
+LightActor::LightActor()
+{
+}
+
+ACTOR_TYPES actor_types[] = {
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    PRIMITIVE_ACTOR,
+    MODEL_ACTOR,
+    MODEL_ACTOR,
+    LIGHT_ACTOR,
+    LIGHT_ACTOR,
+    LIGHT_ACTOR,
+    LIGHT_ACTOR,
+    CAMERA_ACTOR,
+};
+
+std::string resource_dir = "resources/";
+
+std::string template_actor_filepath[] = {
+    "primitives/2D/triangle.2d",
+    "primitives/2D/rectangle.2d",
+    "primitives/2D/quad.2d",
+    "primitives/3D/cube.3d",
+    "primitives/3D/quad.3d",
+    "primitives/3D/plane.3d",
+    "primitives/3D/cubemap.3d",
+    "models/backpack/backpack.obj",
+    "models/sphere/sphere.obj",
+    " ",
+    " ",
+    " ",
+    " ",
+    " "};
+
+SHADER_TYPE default_actor_shaders[] = {
+    COLOR_2D,
+    COLOR_2D,
+    COLOR_2D,
+    COLOR_3D,
+    COLOR_3D,
+    COLOR_3D,
+    COLOR_3D,
+    COLOR_3D,
+    COLOR_3D,
+    LIGHT_SHADER,
+    LIGHT_SHADER,
+    LIGHT_SHADER,
+    LIGHT_SHADER,
+    COLOR_2D};
+
+unsigned int default_actor_id[] = {
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13};
